7fbbdf27a09f00b958de917762907db7fbf94383
Maurício <72310791+mauricio8989@users.noreply.github.com>
2024-10-22 20:45:47 -0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -157,5 +157,5 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mauricio7979.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost," "127.0.0.1", "mauricio7979.pythonanywhere.com"]
 
